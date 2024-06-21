fd9b958472d53c042de7a6e229e76a1c1956e1b4
JoungBin <jeongvin1999@gmail.com>
2024-06-21 11:41:14 +0900

    add result

--- infer/lib/audio.py
+++ infer/lib/audio.py
@@ -1,4 +1,52 @@
-import platform
+import platform, os
 import ffmpeg
 import numpy as np
 import av
+from io import BytesIO
+
+
+def wav2(i, o, format):
+    inp = av.open(i, "rb") # open file in reading mode
+    if format == "m4a":
+        format = "mp4"
+    out = av.open(o, "wb", format=format) # open output file in write mode
+    if format == "ogg":
+        format = "libvorbis"
+    if format == "mp4":
+        format = "aac"
+
+    ostream = out.add_stream(format) #add stream
+
+    # decoding input, incoding to output file
+    for frame in inp.decode(audio=0):
+        for p in ostream.encode(frame):
+            out.mux(p)
+
+    for p in ostream.encode(None):
+        out.mux(p)
+
+    out.close()
+    inp.close()
+
+#read file to return numpy arr
+def load_audio(file, sr):
+    try:
+        # https://github.com/openai/whisper/blob/main/whisper/audio.py#L26
+        # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
+        # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
+        file = clean_path(file)  
+        out, _ = (
+            ffmpeg.input(file, threads=0)
+            .output("-", format="f32le", acodec="pcm_f32le", ac=1, ar=sr) # std output, 32bitf, ,channel, sample frq 
+            .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
+        )
+    except Exception as e:
+        raise RuntimeError(f"Failed to load audio: {e}")
+
+    return np.frombuffer(out, np.float32).flatten()
+
+#Prevent the copy path from containing spaces, " and carriage returns" at the beginning and end
+def clean_path(path_str):
+    if platform.system() == "Windows":
+        path_str = path_str.replace("/", "\\")
+    return path_str.strip(" ").strip('"').strip("\n").strip('"').strip(" ")
